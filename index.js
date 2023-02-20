98788a0f7b606d30a6286ad56d8239bb6f8b07a1
Naeem Hassan <nhnansari@gmail.com>
2023-02-20 22:53:02 +0500

    first push

--- index.js
+++ index.js
@@ -2,14 +2,32 @@ const { isUpper, strToUpper, removePrefix } = require('./src/String');
 const core = require('@actions/core');
 const fs = require('fs');
 const path = require('path');
+const readline = require('readline');
 
 
 const file = core.getInput('file');
 const prefix = strToUpper(core.getInput('prefix'));
 const env = Object.keys(process.env)
     .filter(key => key.startsWith(prefix) && isUpper(key))
-    .map(key => `${removePrefix(key, prefix)}="${process.env[key]}"`)
-    .join('\n');
+    .map(key => `${removePrefix(key, prefix)}="${process.env[key]}"`);
+   // .join('\n');
+
+core.setOutput('ENVArray: ', env);
+
+let textcontent=''
+let filecontent = fs.readFileSync(filePath)
+var FileEnv = filecontent.split("\r\n");
+env.forEach((value,index) => {
+    for (var i = 0; i < FileEnv.length; i++)
+    {
+        let thisline = FileEnv[i]
+        let startstring = value + "="
+        if(thisline.startsWith(startstring)){
+            textcontent += startstring
+        }
+        // Do something with arr
+    }
+});
 
 const workspace = process.env.GITHUB_WORKSPACE;
 const filePath = path.join(workspace, file);
