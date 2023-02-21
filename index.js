4b908c0afe0ed6733c60007069ef69bd339ec84f
Naeem Hassan <nhnansari@gmail.com>
2023-02-21 16:09:50 +0500

    File inputs

--- index.js
+++ index.js
@@ -2,28 +2,25 @@ const { isUpper, strToUpper, removePrefix } = require('./src/String');
 const core = require('@actions/core');
 const fs = require('fs');
 const path = require('path');
-const readline = require('readline');
 
 
-const file = core.getInput('file');
+const examplefile = core.getInput('examplefile');
+const targetfile = core.getInput('targetfile');
 const prefix = strToUpper(core.getInput('prefix'));
 
 const workspace = process.env.GITHUB_WORKSPACE;
-const filePath = path.join(workspace, file);
-
+const targetfilePath = path.join(workspace, targetfile);
+const examplefilePath = path.join(workspace, examplefile);
 
 const env = Object.keys(process.env)
     .filter(key => key.startsWith(prefix) && isUpper(key))
     .map(key => `${removePrefix(key, prefix)}="${process.env[key]}"`);
    // .join('\n');
 
-core.setOutput('ENVArray: ', env);
-let FileEnv = ''
+ let FileEnv = ''
 let textcontent=''
-const data = fs.readFileSync('./.env.example', {encoding:'utf8', flag:'r'});
+const data = fs.readFileSync(examplefilePath, {encoding:'utf8', flag:'r'});
 FileEnv = data.split(/\r?\n/)
-
-console.log("Total Lines", FileEnv.length)
 
 for (var i = 0; i < FileEnv.length; i++)
 {
@@ -45,11 +42,11 @@ for (var i = 0; i < FileEnv.length; i++)
  }
 
  //fs.writeFile(filePath, env, err => {
-fs.writeFile(filePath, textcontent, err => {
+fs.writeFile(targetfilePath, textcontent, err => {
     if (err) {
         core.setFailed(err.message);
     } else {
-        core.setOutput('created: ', file);
+        core.setOutput('created: ', targetfilePath);
     }
 });
 console.log("======= ENV Created =======")
